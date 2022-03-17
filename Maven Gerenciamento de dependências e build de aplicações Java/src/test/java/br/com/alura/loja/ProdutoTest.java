42110b54421a717a88e3ea1e76d5803033c54cdf
CleuJunior <cleu.junior@gmail.com>
2022-03-16 23:20:24 -0300

    maven: plugins maven

--- Maven Gerenciamento de dependências e build de aplicações Java/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ Maven Gerenciamento de dependências e build de aplicações Java/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,13 +1,17 @@
 package br.com.alura.loja;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
+		Produto produto = new Produto("Produto novo", BigDecimal.TEN);
+		Assert.assertEquals("Produto novo", produto.getNome());
+		Assert.assertEquals(BigDecimal.TEN, produto.getPreco());
 
 	}
 
